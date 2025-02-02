153466335b4cf554a1d279eff018e9cd68342efa
seongwon980 <poiuy98749@yonsei.ac.kr>
2025-02-02 12:54:44 +0900

    feat: multi2 & hetero grad sim

--- federated_methods/fedpq.py
+++ federated_methods/fedpq.py
@@ -311,6 +311,137 @@ def fedMultipq_tv_load_state_dict(model, global_state_dict, local_state_dict_lis
         else:
             model.load_state_dict(new_global_state_dict, strict=False) 
 
+def fedMulti2pq_load_state_dict(model, global_state_dict, local_state_dict_list, client_id, training_args, extra_state_dict_dict):
+    # first load loca model and then load global model
+    with torch.no_grad():
+        if 'zero3' in training_args.deepspeed:
+            load_deepspeed(local_state_dict_list[client_id], model, strict=False)
+        else:
+            model.load_state_dict(local_state_dict_list[client_id], strict=False)    
+        
+        cur_layer_num = []
+        for k in global_state_dict.keys():
+            if 'layers.' in k:
+                cur_layer_num.append(int(k.split('.')[5]))
+        cur_layer_num = sorted(list(set(cur_layer_num)))
+        new_global_state_dict = {}
+        for name in global_state_dict.keys():
+            new_param = 0
+            target_key = name
+            
+            for id in range(training_args.num_clients):
+                # if layer number is different
+                splited = target_key.split('.')
+                # if layer number is different
+                layer_num = []
+                for k in local_state_dict_list[id].keys():
+                    if 'layers.' in k:
+                        layer_num.append(int(k.split('.')[5]))
+                layer_num = len(set(layer_num)) // 4
+                
+                target_layers = [layer_num*1 -1,layer_num*2 -1,layer_num*3 -1,layer_num*4 -1]
+                if cur_layer_num[-1] != target_layers[-1]: # if different size
+                    if int(splited[5]) == cur_layer_num[0]: # mid layer
+                        splited[5] = str(target_layers[0])
+                    elif int(splited[5]) == cur_layer_num[1]: # last layer 
+                        splited[5] = str(target_layers[1])
+                    elif int(splited[5]) == cur_layer_num[2]: # last layer 
+                        splited[5] = str(target_layers[2])
+                    elif int(splited[5]) == cur_layer_num[3]: # last layer 
+                        splited[5] = str(target_layers[3])
+                    elif int(splited[5]) == cur_layer_num[4]: # last layer 
+                        splited[5] = str(target_layers[4])
+                    elif int(splited[5]) == cur_layer_num[5]: # last layer 
+                        splited[5] = str(target_layers[5])
+                    elif int(splited[5]) == cur_layer_num[6]: # last layer 
+                        splited[5] = str(target_layers[6])
+                    elif int(splited[5]) == cur_layer_num[7]: # last layer 
+                        splited[5] = str(target_layers[7])
+                    new_target_key = '.'.join(splited)
+                else:
+                    new_target_key = target_key
+                new_param += local_state_dict_list[id][new_target_key] / training_args.num_clients
+                
+            new_global_state_dict[name] = new_param
+            # if (training_args.local_rank == 0 or training_args.local_rank == -1):
+            #     output_dir = os.path.join(training_args.state_dir, f"{client_id}_client_global_model_round{extra_state_dict_dict['curr_round']}.pth")
+            #     torch.save(new_global_state_dict, output_dir)
+        # else:
+        #     new_global_state_dict = global_state_dict
+        if 'zero3' in training_args.deepspeed:
+            load_deepspeed(new_global_state_dict, model, strict=False)
+        else:
+            model.load_state_dict(new_global_state_dict, strict=False)
+
+def fedMulti2pq_tv_load_state_dict(model, global_state_dict, local_state_dict_list, client_id, training_args, extra_state_dict_dict):
+    # first load loca model and then load global model
+    with torch.no_grad():
+        if 'zero3' in training_args.deepspeed:
+            load_deepspeed(local_state_dict_list[client_id], model, strict=False)
+        else:
+            model.load_state_dict(local_state_dict_list[client_id], strict=False)    
+        
+        # gradient based similarity wegithed averaging (exclude own)
+        if extra_state_dict_dict['curr_round'] > 0 and 'task_similarity' in extra_state_dict_dict:
+            # similarity matrix
+            sim = extra_state_dict_dict['task_similarity']
+            
+            weights = sim[client_id].clone()
+            
+            # weights[client_id] = -1e9
+            weights = (weights).softmax(dim=0)
+            
+            sim_sum = weights.sum() #- weights[client_id]
+            
+            # # weights[client_id] = sim_sum
+            # # sim_sum += sim_sum
+            cur_layer_num = []
+            for k in global_state_dict.keys():
+                if 'layers.' in k:
+                    cur_layer_num.append(int(k.split('.')[5]))
+            cur_layer_num = sorted(list(set(cur_layer_num)))
+            new_global_state_dict = {}
+            for name in global_state_dict.keys():
+                new_param = 0
+                for id in range(training_args.num_clients):
+                    # if layer number is different
+                    splited = name.split('.')
+                    layer_num = []
+                    for k in local_state_dict_list[id].keys():
+                        if 'layers.' in k:
+                            layer_num.append(int(k.split('.')[5]))
+                    layer_num = len(set(layer_num)) // 4
+                
+                    target_layers = [layer_num*1 -1,layer_num*2 -1,layer_num*3 -1,layer_num*4 -1]
+                    if cur_layer_num[-1] != target_layers[-1]: # if different size
+                        if int(splited[5]) == cur_layer_num[0]: # mid layer
+                            splited[5] = str(target_layers[0])
+                        elif int(splited[5]) == cur_layer_num[1]: # last layer 
+                            splited[5] = str(target_layers[1])
+                        elif int(splited[5]) == cur_layer_num[2]: # last layer 
+                            splited[5] = str(target_layers[2])
+                        elif int(splited[5]) == cur_layer_num[3]: # last layer 
+                            splited[5] = str(target_layers[3])
+                        elif int(splited[5]) == cur_layer_num[4]: # last layer 
+                            splited[5] = str(target_layers[4])
+                        elif int(splited[5]) == cur_layer_num[5]: # last layer 
+                            splited[5] = str(target_layers[5])
+                        elif int(splited[5]) == cur_layer_num[6]: # last layer 
+                            splited[5] = str(target_layers[6])
+                        elif int(splited[5]) == cur_layer_num[7]: # last layer 
+                            splited[5] = str(target_layers[7])
+                        new_target_key = '.'.join(splited)
+                    else:
+                        new_target_key = name
+                    new_param += weights[id]*local_state_dict_list[id][new_target_key] / sim_sum
+                    
+                new_global_state_dict[name] = new_param
+        else:
+            new_global_state_dict = global_state_dict
+        if 'zero3' in training_args.deepspeed:
+            load_deepspeed(new_global_state_dict, model, strict=False)
+        else:
+            model.load_state_dict(new_global_state_dict, strict=False) 
 
 def feddualpq_load_state_dict(model, global_state_dict, local_state_dict_list, client_id, training_args, extra_state_dict_dict):
     # first load loca model and then load global model
@@ -652,7 +783,7 @@ def feddualMultipq_load_state_dict(model, global_state_dict, local_state_dict_li
         else:
             model.load_state_dict(new_global_state_dict, strict=False) 
 
-def feddualMultipq2_load_state_dict(model, global_state_dict, local_state_dict_list, client_id, training_args, extra_state_dict_dict):
+def feddualMulti2pq_load_state_dict(model, global_state_dict, local_state_dict_list, client_id, training_args, extra_state_dict_dict):
     # first load loca model and then load global model
     with torch.no_grad():
         if 'zero3' in training_args.deepspeed:
@@ -703,10 +834,10 @@ def feddualMultipq2_load_state_dict(model, global_state_dict, local_state_dict_l
                         for k in local_state_dict_list[id].keys():
                             if 'layers.' in k:
                                 layer_num.append(int(k.split('.')[5]))
-                        layer_num = len(set(layer_num)) // 8
+                        layer_num = len(set(layer_num)) // 4
                         
-                        target_layers = [layer_num*1 -1,layer_num*2 -1,layer_num*3 -1,layer_num*4 -1,
-                                         layer_num*5 -1,layer_num*6 -1,layer_num*7 -1,layer_num*8 -1]
+                        target_layers = [layer_num*1 -2, layer_num*1 -1, layer_num*2 -2, layer_num*2 -1,
+                                         layer_num*3 -2, layer_num*3 -1, layer_num*4 -2, layer_num*4 -1]
                         if cur_layer_num[-1] != target_layers[-1]: # if different size
                             if int(splited[5]) == cur_layer_num[0]: # mid layer
                                 splited[5] = str(target_layers[0])
@@ -716,7 +847,7 @@ def feddualMultipq2_load_state_dict(model, global_state_dict, local_state_dict_l
                                 splited[5] = str(target_layers[2])
                             elif int(splited[5]) == cur_layer_num[3]: # last layer 
                                 splited[5] = str(target_layers[3])
-                            elif int(splited[5]) == cur_layer_num[4]: # mid layer
+                            elif int(splited[5]) == cur_layer_num[4]: # last layer 
                                 splited[5] = str(target_layers[4])
                             elif int(splited[5]) == cur_layer_num[5]: # last layer 
                                 splited[5] = str(target_layers[5])
@@ -724,7 +855,6 @@ def feddualMultipq2_load_state_dict(model, global_state_dict, local_state_dict_l
                                 splited[5] = str(target_layers[6])
                             elif int(splited[5]) == cur_layer_num[7]: # last layer 
                                 splited[5] = str(target_layers[7])
-                            new_target_key = '.'.join(splited)
                         else:
                             new_target_key = target_key
                         new_param += weights[id]*local_state_dict_list[id][new_target_key] / sim_sum
@@ -739,4 +869,3 @@ def feddualMultipq2_load_state_dict(model, global_state_dict, local_state_dict_l
             load_deepspeed(new_global_state_dict, model, strict=False)
         else:
             model.load_state_dict(new_global_state_dict, strict=False) 
-
--- federated_methods/method_manager.py
+++ federated_methods/method_manager.py
@@ -9,7 +9,8 @@ from federated_methods.fedpq import (feddualpq_load_state_dict, fedpq_load_state
                                      fedlastpq_load_state_dict, fedFLpq_load_state_dict, feddualFLpq_load_state_dict, 
                                      fedFMLpq_load_state_dict, feddualFMLpq_load_state_dict, feddualMultipq_load_state_dict,
                                      feddualMultipq2_load_state_dict, fedMultipq_load_state_dict,
-                                     fedlastpq_tv_load_state_dict, fedMultipq_tv_load_state_dict)
+                                     fedlastpq_tv_load_state_dict, fedMultipq_tv_load_state_dict,
+                                     feddualMulti2pq_load_state_dict, fedMulti2pq_load_state_dict, fedMulti2pq_tv_load_state_dict)
 
 def dummy_function(*args):
     return {}
@@ -24,16 +25,12 @@ def select_method(mode: str) -> Tuple[Callable, Callable, Callable, Callable, Di
         set_state_dict, load_state_dict, create_trainer, aggregate_state_dict = dummy_function, fedours_load_state_dict, fedours_ema_distill_create_trainer, OURS_aggregate_state_dict  
     elif mode =='fedpq' or mode == 'fedpqfreeze' or mode == 'fedpqfreeze2':
         set_state_dict, load_state_dict, create_trainer, aggregate_state_dict = dummy_function, fedpq_load_state_dict, fedavg_create_trainer, fedavg_aggregate_state_dict
-    elif mode =='fedpq_sft' or mode == 'fedpqfreeze_sft' or mode == 'fedpqfreeze2_sft' or mode =='fedlastpqfullfreeze_sft' or mode == 'fedMultipqfullfreeze_sft':
+    elif mode =='fedpq_sft' or mode == 'fedpqfreeze_sft' or mode == 'fedpqfreeze2_sft' or mode =='fedlastpqfullfreeze_sft' or mode == 'fedMultipqfullfreeze_sft' or mode == 'fedMulti2pqfullfreeze_sft':
         set_state_dict, load_state_dict, create_trainer, aggregate_state_dict = dummy_function, sft_load_state_dict, fedavg_create_trainer, fedavg_aggregate_state_dict
     elif mode == 'fedlastpq' or mode == 'fedlastpqfreeze' or mode =='fedlastpqfullfreeze':
         set_state_dict, load_state_dict, create_trainer, aggregate_state_dict = dummy_function, fedlastpq_load_state_dict, fedavg_create_trainer, fedavg_aggregate_state_dict
     elif mode =='fedlastpqfullfreeze_tv' or mode == 'fedlastpqfullfreeze_ours':
         set_state_dict, load_state_dict, create_trainer, aggregate_state_dict = dummy_function, fedlastpq_tv_load_state_dict, fedavg_create_trainer, fedavg_aggregate_state_dict
-    elif mode == 'fedFLpq' or mode == 'fedFLpqfreeze':
-        set_state_dict, load_state_dict, create_trainer, aggregate_state_dict = dummy_function, fedFLpq_load_state_dict, fedavg_create_trainer, fedavg_aggregate_state_dict
-    elif mode == 'fedFMLpq':
-        set_state_dict, load_state_dict, create_trainer, aggregate_state_dict = dummy_function, fedFMLpq_load_state_dict, fedavg_create_trainer, fedavg_aggregate_state_dict
     elif mode =='feddualpq' or mode == 'feddualpqfullfreeze' or mode == 'feddualpqfullfreeze_tv':
         set_state_dict, load_state_dict, create_trainer, aggregate_state_dict = dummy_function, feddualpq_load_state_dict, fedours_ema_distill_create_trainer, OURS_aggregate_state_dict
     elif mode =='fedduallastpq' or mode == 'fedduallastpqfreeze' or mode == 'fedduallastpqfullfreeze' or mode == 'fedduallastpqfullfreeze_tv':
@@ -44,13 +41,17 @@ def select_method(mode: str) -> Tuple[Callable, Callable, Callable, Callable, Di
         set_state_dict, load_state_dict, create_trainer, aggregate_state_dict = dummy_function, feddualFMLpq_load_state_dict, fedours_ema_distill_create_trainer, OURS_aggregate_state_dict
     elif mode =='feddualMultipqfreeze' or mode =='feddualMultipqfullfreeze' or mode == 'feddualMultipqfullfreeze_tv':
         set_state_dict, load_state_dict, create_trainer, aggregate_state_dict = dummy_function, feddualMultipq_load_state_dict, fedours_ema_distill_create_trainer, OURS_aggregate_state_dict
-    elif mode =='feddualMultipqfreeze2':
-        set_state_dict, load_state_dict, create_trainer, aggregate_state_dict = dummy_function, feddualMultipq2_load_state_dict, fedours_ema_distill_create_trainer, OURS_aggregate_state_dict
-    
+    elif mode =='feddualMulti2pqfullfreeze' or mode == 'feddualMulti2pqfullfreeze_tv':
+        set_state_dict, load_state_dict, create_trainer, aggregate_state_dict = dummy_function, feddualMulti2pq_load_state_dict, fedours_ema_distill_create_trainer, OURS_aggregate_state_dict
+
     elif mode =='fedMultipqfullfreeze':
         set_state_dict, load_state_dict, create_trainer, aggregate_state_dict = dummy_function, fedMultipq_load_state_dict, fedavg_create_trainer, OURS_aggregate_state_dict
     elif mode =='fedMultipqfullfreeze_tv' or 'fedMultipqfullfreeze_ours':
         set_state_dict, load_state_dict, create_trainer, aggregate_state_dict = dummy_function, fedMultipq_tv_load_state_dict, fedavg_create_trainer, OURS_aggregate_state_dict
+    elif mode =='fedMulti2pqfullfreeze':
+        set_state_dict, load_state_dict, create_trainer, aggregate_state_dict = dummy_function, fedMulti2pq_load_state_dict, fedavg_create_trainer, OURS_aggregate_state_dict
+    elif mode =='fedMulti2pqfullfreeze_tv' or 'fedMulti2pqfullfreeze_ours':
+        set_state_dict, load_state_dict, create_trainer, aggregate_state_dict = dummy_function, fedMulti2pq_tv_load_state_dict, fedavg_create_trainer, OURS_aggregate_state_dict
     
     
     elif mode =='fedavg_memefficient':

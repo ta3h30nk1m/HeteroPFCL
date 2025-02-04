0aa376bbb3e1360fe6d55a986e5edd9be2d78b9e
ta3h30nk1m <ta3h30nk1m@gmail.com>
2025-02-04 13:20:45 +0000

    feat: client-wise matrix csv

---
+++ combined_score_matrix.py
@@ -0,0 +1,69 @@
+import jsonlines
+import json
+import csv
+from collections import defaultdict
+import os
+import numpy as np
+
+mode = 'sft'
+Method='sft_bs4_saveoptim_lr2e-5_sc80_4tasks_5rounds_fixitr100_T0125_decay099'
+
+num_rounds = [20]
+is_client = True
+
+scenario_num = 80
+with open(f"./scenarios/scenario-{scenario_num}.json") as fp:
+    scenario = json.load(fp)
+
+for num_round in num_rounds:
+    results = []
+    for client_id in range(len(scenario)):
+        client_results = []
+        for client_data in scenario:
+            task_score = 0
+            id = client_data['client_id']
+            for data in client_data['datasets']:
+                data_name = f"{data['dataset']}-{data['subset_id']}"
+                if is_client:
+                    with open(f'./eval_results/{mode}/{Method}/client{client_id}_round{num_round}_{data_name}.json', 'r') as fp:
+                        result = json.load(fp)[-1]
+                else:
+                    with open(f'./eval_results/{mode}/{Method}/server_round{num_round}_{data_name}.json', 'r') as fp:
+                        result = json.load(fp)[-1]
+                
+                if data['type'] == 'multi-choice':
+                    score = result['accuracy']
+                elif data['type'] == 'open-ended':
+                    if data['metric'] == 'F1':
+                        score = 2*(result['precision']*result['recall']) / (result['precision'] + result['recall'])
+                    elif data['metric'] == 'RougeL':
+                        score = result['ROUGE_L'][0]
+                
+                task_score += score
+            client_results.append(task_score/len(client_data['datasets']))
+        results.append(client_results)
+    
+    # Prepare data for CSV
+    csv_datas = []
+    for i, (result) in enumerate(results):
+        csv_data = [Method, num_round]
+        csv_data.extend(result[:])
+        csv_datas.append(csv_data)
+
+    # Prepare header
+    header = ['method', 'num_round']
+    header.extend([f'client {i}' for i in range(10)])
+
+    # Write to CSV
+    csv_file = f'{Method}.csv'
+    file_exists = os.path.isfile(csv_file)
+
+    with open(csv_file, 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        if not file_exists:
+            writer.writerow(header)
+        for csv_data in csv_datas:
+            writer.writerow(csv_data)
+
+    print(f"Method: {Method}")
+    print(f"Results have been appended to {csv_file}")

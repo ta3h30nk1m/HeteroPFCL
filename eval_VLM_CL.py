59c06cff9a03769abd3b901635344a5fec266235
d <d>
2025-03-11 16:24:04 +0900

    update eval

--- eval_VLM_CL.py
+++ eval_VLM_CL.py
@@ -428,7 +428,7 @@ def main():
                 model.load_state_dict(client_state_dict, strict=False)
                 model = model.to(torch.bfloat16)
                 model = model.to(device)
-            if training_args.mode in ['fedours', 'fedours_tv', 'fedours_excludemean'] or 'dual' in training_args.mode:
+            if 'fedours' in training_args.mode or 'dual' in training_args.mode:
                 model.set_state('gate')
             
             for client_id_ in range(training_args.num_clients):
@@ -469,7 +469,7 @@ def main():
                     personal_global_state_dict = torch.load(f'./client_states_{training_args.note}/{client_id}_client_global_model_round{training_args.round_to_eval}.pth', map_location='cpu')
                     model.load_state_dict(personal_global_state_dict, strict=False)
             # model.load_state_dict(server_state_dict, strict=False)
-            if training_args.mode in ['fedours', 'fedours_tv', 'fedours_excludemean', 'fedours_include', 'fedours_moe', 'fedours_excludemean_hetero'] or 'dual' in training_args.mode:
+            if 'fedours' in training_args.mode or 'dual' in training_args.mode:
                 # for name, module in model.named_modules():
                 #     if isinstance(module, DualLoraLayer) or isinstance(module, DualIA3Layer):
                 #         module.set_state('lora2')
@@ -492,7 +492,7 @@ def main():
             if training_args.eval_server and data_info['data_name'] not in server_eval_key:
                 if not training_args.zeroshot:
                     model.load_state_dict(server_state_dict, strict=False)
-                if training_args.mode in ['fedours', 'fedours_tv'] or 'dual' in training_args.mode:
+                if 'fedours' in training_args.mode or 'dual' in training_args.mode:
                     # for name, module in model.named_modules():
                     #     if isinstance(module, DualLoraLayer) or isinstance(module, DualIA3Layer):
                     #         module.set_state('lora1')

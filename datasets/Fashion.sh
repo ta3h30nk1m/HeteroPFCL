346eb0aa3483c07b72929c88267d4756b5fa0138
Diganta Misra <mishradiganta91@gmail.com>
2025-03-26 10:29:51 -0400

    Fashion updated

--- datasets/Fashion.sh
+++ datasets/Fashion.sh
@@ -1,7 +1,13 @@
-gdrive files download 193yftaEWSR8fwjvc9p-sqBsaZlVo4dNS
-unzip Fashion200K.zip
-rm Fashion200K.zip
+#./gdrive files download 193yftaEWSR8fwjvc9p-sqBsaZlVo4dNS
+#unzip Fashion200K.zip
+#rm Fashion200K.zip
 
-gdrive files download 1SoT4zScEf3g1Y0FeGpiL4YIk4A-ySLbN
-tar -xvf FashionIQ.tar
-rm FashionIQ.tar
+./gdrive files download 1JFvucHGjKDBkjIhaBJ1hbrdWwZUDR6gQ
+tar -xvf fashion200k_train_test.tar
+mv fashion200k_train_test/* Fashion200K
+rm -rf fashion200k_train_test
+rm fashion200k_train_test.tar
+
+#./gdrive files download 1SoT4zScEf3g1Y0FeGpiL4YIk4A-ySLbN
+#tar -xvf FashionIQ.tar
+#rm FashionIQ.tar

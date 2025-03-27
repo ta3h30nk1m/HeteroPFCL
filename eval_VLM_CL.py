4dbd70a6369720aa2002c8095ec2103b2d88061e
d <d>
2025-03-27 15:12:59 +0900

    update eval for feddat&perada

--- eval_VLM_CL.py
+++ eval_VLM_CL.py
@@ -473,8 +473,8 @@ def main():
                 model.load_state_dict(client_state_dict, strict=False)
                 model = model.to(torch.bfloat16)
                 model = model.to(device)
-            if 'fedours' in training_args.mode or 'dual' in training_args.mode or 'fedquad' in training_args.mode or 'fedhexa' in training_args.mode:
-                model.set_state('gate')
+            if 'fedours' in training_args.mode or 'dual' in training_args.mode or 'fedquad' in training_args.mode or 'fedhexa' in training_args.mode or 'feddat' in training_args.mode or 'perada' in training_args.mode:
+                model.set_state(training_args.set_state)
             
             for client_id_ in range(training_args.num_clients):
                 test_datalist_ = test_datalists[client_id_]
@@ -514,11 +514,11 @@ def main():
                     personal_global_state_dict = torch.load(f'./client_states_{training_args.note}/{client_id}_client_global_model_round{training_args.round_to_eval}.pth', map_location='cpu')
                     model.load_state_dict(personal_global_state_dict, strict=False)
             # model.load_state_dict(server_state_dict, strict=False)
-            if 'fedours' in training_args.mode or 'dual' in training_args.mode or 'fedquad' in training_args.mode or 'fedhexa' in training_args.mode:
+            if 'fedours' in training_args.mode or 'dual' in training_args.mode or 'fedquad' in training_args.mode or 'fedhexa' in training_args.mode or 'feddat' in training_args.mode or 'perada' in training_args.mode:
                 # for name, module in model.named_modules():
                 #     if isinstance(module, DualLoraLayer) or isinstance(module, DualIA3Layer):
                 #         module.set_state('lora2')
-                model.set_state('gate')
+                model.set_state(training_args.set_state)
                 # model.base_model.model.model.mm_projector = model.base_model.model.model.local_mm_projector
             dataset = GenerationDataset(data_info['data'], tokenizer, data_args, processor)
             
@@ -538,11 +538,11 @@ def main():
             if training_args.eval_server and data_info['data_name'] not in server_eval_key:
                 if not training_args.zeroshot:
                     model.load_state_dict(server_state_dict, strict=False)
-                if 'fedours' in training_args.mode or 'dual' in training_args.mode or 'fedquad' in training_args.mode or 'fedhexa' in training_args.mode:
+                if 'fedours' in training_args.mode or 'dual' in training_args.mode or 'fedquad' in training_args.mode or 'fedhexa' in training_args.mode or 'feddat' in training_args.mode or 'perada' in training_args.mode:
                     # for name, module in model.named_modules():
                     #     if isinstance(module, DualLoraLayer) or isinstance(module, DualIA3Layer):
                     #         module.set_state('lora1')
-                    model.set_state('gate')
+                    model.set_state(training_args.set_state)
                     # model.base_model.model.model.mm_projector = model.base_model.model.model.global_mm_projector
             # #     if data_info['data_name'] in CHOICE_DATA: 
             # #         evaluate_choices(dataset, data_info['data_name'], training_args.round_to_eval, model, tokenizer, device, model_args, training_args, logger, None)

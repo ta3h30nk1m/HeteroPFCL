8135208d61fb5fe22b22b5faa1954a32bf688aba
d <d>
2025-03-28 21:36:51 +0900

    AQUA updated

--- datasets/AQUA_feddat.sh
+++ datasets/AQUA_feddat.sh
@@ -7,10 +7,9 @@ rm -rf SemArt
 git clone https://github.com/noagarcia/ArtVQA
 mv ArtVQA/AQUA/*.json AQUA
 rm -rf ArtVQA
-cd AQUA
 mv AQUA/Images AQUA/images
 
-./gdrive files download 1VLX4WwmE6tYhKK7vK1lSNZb5eFt64p53
+gdrive files download 1VLX4WwmE6tYhKK7vK1lSNZb5eFt64p53
 tar -xvf AQUA_feddat_jsons.tar
 mv train AQUA
 mv test AQUA

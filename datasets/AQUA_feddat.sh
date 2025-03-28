19a082ca0b25617a1f3eb673c0108d38aea06ff8
vsc37545 <vsc37545@tier2-p-login-4.genius.hpc.kuleuven.be>
2025-03-28 12:33:58 +0100

    AQUA updated

--- datasets/AQUA_feddat.sh
+++ datasets/AQUA_feddat.sh
@@ -8,8 +8,10 @@ git clone https://github.com/noagarcia/ArtVQA
 mv ArtVQA/AQUA/*.json AQUA
 rm -rf ArtVQA
 cd AQUA
-mv Images images
+mv AQUA/Images AQUA/images
 
 ./gdrive files download 1VLX4WwmE6tYhKK7vK1lSNZb5eFt64p53
 tar -xvf AQUA_feddat_jsons.tar
+mv train AQUA
+mv test AQUA
 rm AQUA_feddat_jsons.tar

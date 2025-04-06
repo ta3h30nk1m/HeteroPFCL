d9c312b9ab3406c95056c444b1a759335ddfc9a0
d <d>
2025-04-07 00:29:16 +0900

    feat: split8 incomplete

---
+++ preprocess_again_split8/preprocess_again.py
@@ -0,0 +1,65 @@
+import json
+import random
+import shutil
+random.seed(42)
+
+# client 5
+original_data = json.load(open('dataset/imagecode/train/dataset-1.json','r'))
+sampled_data = random.sample(original_data, 5000)
+with open('dataset/imagecode/train/dataset-51.json','w') as fp:
+    json.dump(sampled_data, fp, indent=4)
+original_data = json.load(open('dataset/imagecode/test/dataset-1.json','r'))
+sampled_data = random.sample(original_data, 1000)
+with open('dataset/imagecode/test/dataset-51.json','w') as fp:
+    json.dump(sampled_data, fp, indent=4)
+
+# client 8 #######################################################################################
+shutil.copyfile('dataset/dvqa/train/dataset-0.json', 'dataset/dvqa/train/dataset-50.json')
+original_data = json.load(open('dataset/dvqa/test/dataset-0.json','r'))
+sampled_data = random.sample(original_data, 1000)
+with open('dataset/dvqa/test/dataset-50.json','w') as fp:
+    json.dump(sampled_data, fp, indent=4)
+
+shutil.copyfile('dataset/WCVQA/train/dataset-3.json', 'dataset/WCVQA/train/dataset-53.json')
+shutil.copyfile('dataset/WCVQA/test/dataset-3.json', 'dataset/WCVQA/test/dataset-53.json')
+
+# <new dataset>
+
+# client 9 #######################################################################################
+shutil.copyfile('dataset/TQA/train/dataset-0.json', 'dataset/TQA/train/dataset-50.json')
+original_data = json.load(open('dataset/TQA/test/dataset-0.json','r'))
+sampled_data = random.sample(original_data, 1000)
+with open('dataset/TQA/test/dataset-50.json','w') as fp:
+    json.dump(sampled_data, fp, indent=4)
+    
+shutil.copyfile('dataset/VSR/train/dataset-0.json', 'dataset/VSR/train/dataset-50.json')
+shutil.copyfile('dataset/VSR/test/dataset-0.json', 'dataset/VSR/test/dataset-50.json')
+
+# <new dataset>
+
+shutil.copyfile('dataset/iconqa/train/dataset-3.json', 'dataset/iconqa/train/dataset-53.json')
+original_data = json.load(open('dataset/iconqa/test/dataset-3.json','r'))
+sampled_data = random.sample(original_data, 1000)
+with open('dataset/iconqa/test/dataset-53.json','w') as fp:
+    json.dump(sampled_data, fp, indent=4)
+
+# client 10 #######################################################################################
+shutil.copyfile('dataset/iconqa/train/dataset-0.json', 'dataset/iconqa/train/dataset-50.json')
+original_data = json.load(open('dataset/iconqa/test/dataset-0.json','r'))
+sampled_data = random.sample(original_data, 1000)
+with open('dataset/iconqa/test/dataset-50.json','w') as fp:
+    json.dump(sampled_data, fp, indent=4)
+
+# <new dataset>
+
+shutil.copyfile('dataset/SEED-Bench-2/train/dataset-0.json', 'dataset/SEED-Bench-2/train/dataset-50.json')
+original_data = json.load(open('dataset/SEED-Bench-2/test/dataset-0.json','r'))
+sampled_data = random.sample(original_data, 1000)
+with open('dataset/SEED-Bench-2/test/dataset-50.json','w') as fp:
+    json.dump(sampled_data, fp, indent=4)
+
+original_data = json.load(open('dataset/WCVQA/train/dataset-2.json','r'))
+sampled_data = random.sample(original_data, 8000)
+with open('dataset/WCVQA/train/dataset-42.json','w') as fp:
+    json.dump(sampled_data, fp, indent=4)
+shutil.copyfile('dataset/WCVQA/test/dataset-2.json', 'dataset/WCVQA/test/dataset-42.json')

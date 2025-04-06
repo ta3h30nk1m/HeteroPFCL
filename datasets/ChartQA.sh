593f2023ba2739bdea0b963d188d375ef363418e
ta3h30nk1m <ta3h30nk1m@gmail.com>
2025-04-07 01:13:04 +0900

    ChartQA pathVQA added

---
+++ datasets/ChartQA.sh
@@ -0,0 +1,3 @@
+gdrive files download 1b2ySNMmDjDj47sQqDQp0u5PU-5Lq6tGh
+tar -xvf ChartQA.tar
+rm ChartQA.tar
---
+++ datasets/PathVQA.sh
@@ -0,0 +1,3 @@
+gdrive files download 15rinlHbZf4UknZBq3sxN9TlzCE1Wda84
+tar -xvf PathVQA.tar
+rm PathVQA.tar
